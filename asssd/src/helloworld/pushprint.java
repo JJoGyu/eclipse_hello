caa5d0c4c56f1bfd1e7f3ded25623778bab33ed1
JJoGyu <102271684+JJoGyu@users.noreply.github.com>
2022-04-07 11:05:15 +0900

    Update pushprint.java
    PushPrint.java modified on GitHub

--- asssd/src/helloworld/pushprint.java
+++ asssd/src/helloworld/pushprint.java
@@ -1,4 +1,8 @@
 
-public class pushprint {
-
+public class pushprint 
+{
+  pubilc static void main (String[] args) 
+  {
+    System.out.print("Add Java file");
+    System.out.print("Hello...");
 }
